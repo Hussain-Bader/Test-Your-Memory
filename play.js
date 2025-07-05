98cfea2dfd86543504cda2812c036e8f5adedf94
Hussain-Bader <7ussain.3ali@gmail.com>
2025-07-05 14:52:27 +0300

    make function to the game engine to store all functions on it

--- play.js
+++ play.js
@@ -31,31 +31,31 @@ let array = []
 let flipArray = []
 let sameArray = 0
 const TsameArray = 2
-const objct = {
-  cat: 'img/cat.png',
-  dog: 'img/dog.png'
+function game() {
+  create()
+  random()
 }
-let i
-for (i = 0; i < 4; ++i) {
-  array.push(i)
-}
-console.log(array)
-function add() {
-  const container = document.querySelector('#container')
-  array.forEach((num) => {
-    const createBoxes = document.createElement('div')
-    const createImage = document.createElement('img')
+function create() {
+  for (let i = 0; i < 4; ++i) {
+    array.push(i)
+  }
+  console.log(array)
+  function add() {
+    const container = document.querySelector('#container')
+    array.forEach((num) => {
+      const createBoxes = document.createElement('div')
 
-    createBoxes.setAttribute('class', 'box')
-    return container.append(createBoxes)
-  })
-}
-add()
-if (point >= 10) {
+      createBoxes.setAttribute('class', 'box')
+      return container.append(createBoxes)
+    })
+  }
   add()
-}
-console.log(array)
-if (point >= 30) {
-  add()
-  add()
+  if (point >= 10) {
+    add()
+  }
+  console.log(array)
+  if (point >= 30) {
+    add()
+    add()
+  }
 }
